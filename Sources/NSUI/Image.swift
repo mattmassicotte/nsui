c5e98346977d82b3a14597e37d960e99adc3e763
Kaï-Zen B. Šæmañn <k@izen.one>
2023-10-16 11:15:58 -0400

    Image (#2)
    * Image
    
    * Make ciImage public

---
+++ Sources/NSUI/Image.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+#if canImport(AppKit)
+public typealias NSUIImage = NSImage
+#elseif canImport(UIKit)
+public typealias NSUIImage = UIImage
+#endif
+
+extension Image {
+    public init(nsuiImage: NSUIImage) {
+#if canImport(AppKit)
+            self.init(nsImage: nsuiImage)
+#elseif canImport(UIKit)
+            self.init(uiImage: nsuiImage)
+#endif
+    }
+}
+
+#if canImport(AppKit)
+extension NSImage {
+    public var cgImage: CGImage? {
+        guard let imageData = self.tiffRepresentation,
+           let bitmap = NSBitmapImageRep(data: imageData) else {
+            return nil
+        }
+        return bitmap.cgImage
+    }
+    
+    @available(macOS 11.0, *)
+    public convenience init?(systemName: String) {
+        self.init(systemSymbolName: systemName, accessibilityDescription: systemName)
+    }
+}
+#endif
