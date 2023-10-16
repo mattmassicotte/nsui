6fbc67e1edc8196fde9bdc1c34a81cd068aacd1e
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-10-16 11:17:14 -0400

    indentation

--- Package.swift
+++ Package.swift
@@ -3,19 +3,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "NSUI",
+	name: "NSUI",
 	platforms: [
 		.macOS(.v10_15),
 		.iOS(.v13),
 		.tvOS(.v13),
 	],
-    products: [
-        .library(name: "NSUI", targets: ["NSUI"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(name: "NSUI", dependencies: []),
-        .testTarget(name: "NSUITests", dependencies: ["NSUI"]),
-    ]
+	products: [
+		.library(name: "NSUI", targets: ["NSUI"]),
+	],
+	dependencies: [
+	],
+	targets: [
+		.target(name: "NSUI", dependencies: []),
+		.testTarget(name: "NSUITests", dependencies: ["NSUI"]),
+	]
 )
