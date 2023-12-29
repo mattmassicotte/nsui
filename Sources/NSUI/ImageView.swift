4d16817e789b05f7cacd7df8396dcdff61171c96
Martin Dufort <martin.dufort@gmail.com>
2023-12-29 11:20:52 -0500

    Make tintColor a variable setter for NSUIImageView(AppKit) as to not break UIKit logic.

--- Sources/NSUI/ImageView.swift
+++ Sources/NSUI/ImageView.swift
@@ -2,17 +2,25 @@ import SwiftUI
 
 #if canImport(AppKit)
 public extension NSImageView {
-    func tintColor(color: NSUIColor) {
-        guard
-            let selfImage = self.image
-        else { return }
-        let image = NSImage(size: selfImage.size, flipped: false) { (rect) -> Bool in
-            color.set()
-            rect.fill()
-            selfImage.draw(in: rect, from: NSRect(origin: .zero, size: selfImage.size), operation: .destinationIn, fraction: 1.0)
-            return true
+    var tintColor: NSUIColor {
+        @available(*, unavailable)
+        get {
+            fatalError("Unavailable")
         }
-        self.image = image
+        set {
+            guard
+                let selfImage = self.image
+            else {
+                return
+            }
+            let image = NSImage(size: selfImage.size, flipped: false) { (rect) -> Bool in
+                newValue.set()
+                rect.fill()
+                selfImage.draw(in: rect, from: NSRect(origin: .zero, size: selfImage.size), operation: .destinationIn, fraction: 1.0)
+                return true
+            }
+            self.image = image
+        }
     }
 }
 #endif
