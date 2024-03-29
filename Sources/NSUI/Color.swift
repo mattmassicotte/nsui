bbfbda6e8d0ea401eab5e2d1e01c5bbdceb7d8b9
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-03-29 08:55:16 -0400

    Fix availability guard

--- Sources/NSUI/Color.swift
+++ Sources/NSUI/Color.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension Color {
-	@available(macOS 12.0, *)
+	@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
 	public init(nsuiColor: NSUIColor) {
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
 		self.init(nsColor: nsuiColor)
