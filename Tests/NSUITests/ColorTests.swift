74ca134ec891ec007c443cc9623659a3bc3e225d
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-03-29 08:59:06 -0400

    Guard for test as well

--- Tests/NSUITests/ColorTests.swift
+++ Tests/NSUITests/ColorTests.swift
@@ -4,6 +4,7 @@ import XCTest
 import NSUI
 
 final class ColorTests: XCTestCase {
+	@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
 	func testInit() {
 		let _ = Color(nsuiColor: NSUIColor.red)
 	}
