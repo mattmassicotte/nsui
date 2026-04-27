66fa118a9b4d5cec244bc45bd4cf78c82868291d
Matt <85322+mattmassicotte@users.noreply.github.com>
2026-04-27 09:03:31 -0400

    Sort macOS aliases

--- Sources/NSUI/Aliases.swift
+++ Sources/NSUI/Aliases.swift
@@ -7,23 +7,20 @@ public typealias NSUIApplicationDelegate = NSApplicationDelegate
 @available(macOS 11.0, *)
 public typealias NSUIApplicationDelegateAdaptor = NSApplicationDelegateAdaptor
 public typealias NSUIBezierPath = NSBezierPath
-// Storyboard
-public typealias NSUIStoryboard = NSStoryboard
-
-// Collection View
+public typealias NSUIButton = NSButton
 public typealias NSUICollectionDataSource = NSCollectionViewDataSource
 public typealias NSUICollectionView = NSCollectionView
-public typealias NSUICollectionViewItem = NSCollectionViewItem
 public typealias NSUICollectionViewDelegate = NSCollectionViewDelegate
-public typealias NSUICollectionViewLayout = NSCollectionViewLayout
-public typealias NSUICollectionViewFlowLayout = NSCollectionViewFlowLayout
-public typealias NSUICollectionViewLayoutAttributes = NSCollectionViewLayoutAttributes
 public typealias NSUICollectionViewDelegateFlowLayout = NSCollectionViewDelegateFlowLayout
-
+public typealias NSUICollectionViewFlowLayout = NSCollectionViewFlowLayout
+public typealias NSUICollectionViewItem = NSCollectionViewItem
+public typealias NSUICollectionViewLayout = NSCollectionViewLayout
+public typealias NSUICollectionViewLayoutAttributes = NSCollectionViewLayoutAttributes
 public typealias NSUIColor = NSColor
 public typealias NSUIEdgeInsets = NSEdgeInsets
 public typealias NSUIFont = NSFont
 public typealias NSUIFontDescriptor = NSFontDescriptor
+public typealias NSUIHostingController = NSHostingController
 public typealias NSUIImage = NSImage
 public typealias NSUIImageView = NSImageView
 public typealias NSUILabel = NSTextField
@@ -31,22 +28,18 @@ public typealias NSUILongPressGestureRecognizer = NSPressGestureRecognizer
 public typealias NSUINib = NSNib
 public typealias NSUIPasteboard = NSPasteboard
 public typealias NSUIResponder = NSResponder
-public typealias NSUITapGestureRecognizer = NSClickGestureRecognizer
+public typealias NSUIStackView = NSStackView
+public typealias NSUIStoryboard = NSStoryboard
+public typealias NSUITableView = NSTableView
 @available(macOS 11.0, *)
 public typealias NSUITableViewDiffableDataSource = NSTableViewDiffableDataSource
-
-// Views
-public typealias NSUIButton = NSButton
-public typealias NSUITableView = NSTableView
+public typealias NSUITapGestureRecognizer = NSClickGestureRecognizer
 public typealias NSUITextField = NSTextField
 public typealias NSUITextFieldDelegate = NSTextFieldDelegate
 public typealias NSUITextView = NSTextView
 public typealias NSUIView = NSView
-public typealias NSUIStackView = NSStackView
 public typealias NSUIViewController = NSViewController
-public typealias NSUIHostingController = NSHostingController
 public typealias NSUIWorkspace = NSWorkspace
-
 #elseif canImport(UIKit)
 public typealias NSUIActivityIndicatorView = UIActivityIndicatorView
 public typealias NSUIApplication = UIApplication
@@ -90,5 +83,4 @@ public typealias NSUITextView = UITextView
 public typealias NSUIView = UIView
 public typealias NSUIViewController = UIViewController
 public typealias NSUIWorkspace = UIApplication
-
 #endif
