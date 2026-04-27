a914e41f62a3ba7609e4252414d06c21f4c877d8
Matt <85322+mattmassicotte@users.noreply.github.com>
2026-04-27 08:45:05 -0400

    Simplify isolation annotations

--- Sources/NSUI/ViewControllerRepresentable.swift
+++ Sources/NSUI/ViewControllerRepresentable.swift
@@ -18,24 +18,21 @@ public protocol NSUIViewControllerRepresentable: NSViewControllerRepresentable w
 	func sizeThatFits(_ proposal: ProposedViewSize, nsuiViewController: Self.NSUIViewControllerType, context: Self.Context) -> CGSize?
 }
 
+@MainActor
 public extension NSUIViewControllerRepresentable {
-	@MainActor
 	func makeNSViewController(context: Self.Context) -> Self.NSUIViewControllerType {
 		makeNSUIViewController(context: context)
 	}
 
-	@MainActor
 	func updateNSViewController(_ viewController: Self.NSUIViewControllerType, context: Self.Context) {
 		updateNSUIViewController(viewController, context: context)
 	}
 
-	@MainActor
 	static func dismantleNSViewController(_ viewController: Self.NSUIViewControllerType, coordinator: Self.Coordinator) {
 		Self.dismantleNSUIViewController(viewController, coordinator: coordinator)
 	}
 
 	@available(macOS 13.0, *)
-	@MainActor
 	func sizeThatFits(_ proposal: ProposedViewSize, nsViewController: Self.NSUIViewControllerType, context: Self.Context) -> CGSize? {
 		sizeThatFits(proposal, nsuiViewController: nsViewController, context: context)
 	}
@@ -59,24 +56,21 @@ public protocol NSUIViewControllerRepresentable: UIViewControllerRepresentable w
 	func sizeThatFits(_ proposal: ProposedViewSize, nsuiViewController: Self.NSUIViewControllerType, context: Self.Context) -> CGSize?
 }
 
+@MainActor
 public extension NSUIViewControllerRepresentable {
-	@MainActor
 	func makeUIViewController(context: Self.Context) -> Self.NSUIViewControllerType {
 		makeNSUIViewController(context: context)
 	}
 
-	@MainActor
 	func updateUIViewController(_ viewController: Self.NSUIViewControllerType, context: Self.Context) {
 		updateNSUIViewController(viewController, context: context)
 	}
 
-	@MainActor
 	static func dismantleUIViewController(_ viewController: Self.NSUIViewControllerType, coordinator: Self.Coordinator) {
 		Self.dismantleNSUIViewController(viewController, coordinator: coordinator)
 	}
 
 	@available(iOS 16.0, tvOS 16.0, *)
-	@MainActor
 	func sizeThatFits(
 		_ proposal: ProposedViewSize,
 		uiViewController: Self.NSUIViewControllerType,
@@ -87,13 +81,12 @@ public extension NSUIViewControllerRepresentable {
 }
 #endif
 
+@MainActor
 extension NSUIViewControllerRepresentable {
-	@MainActor
 	public static func dismantleNSUIViewController(_ viewController: Self.NSUIViewControllerType, coordinator: Self.Coordinator) {
 	}
 
 	@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
-	@MainActor
 	public func sizeThatFits(_ proposal: ProposedViewSize, nsuiViewController: Self.NSUIViewControllerType, context: Self.Context) -> CGSize? {
 		nil
 	}
